04ae880729f14ef65225e1754f0fbaafe6f3b4f6
me1ina <melina.lorenz@atos.net>
2025-10-27 19:36:38 +0100

    physics data v8 inf

--- test_graph_inference.py
+++ test_graph_inference.py
@@ -92,8 +92,8 @@ inference_graph_name = "graph_area_VagusA6050_HC0_AS1.1.dgl"
 #model_name = "trained_gnn_NNConv_dirichlet_v2.pth"
 #model_name = "trained_gnn_NNConv_laplace_v4.pth" 
 #model_name = "trained_gnn_NNConv_laplace_only_8.pth" 
-#model_name = "trained_gnn_NNConv_v7.pth" 
-model_name = "trained_data_physics_2.pth" 
+model_name = "trained_gnn_NNConv_v8.pth" 
+#model_name = "trained_data_physics_2.pth" 
 
 in_feats = 6
 hidden_feats = 64
@@ -174,6 +174,6 @@ g.ndata["Electric_potential"] = preds.squeeze(1)
 end_time = time.time() - start_time
 logging.info(f"Inference completed in {end_time:.3f} seconds, storing results in graph...")
 
-dgl.save_graphs("inference_gnn_combi_VagusA6050_HC0_AS1_2.1.dgl", [g])
-logging.info("Graph saved to inference_gnn_combi_VagusA6050_HC0_AS1_2.1.dgl")
+dgl.save_graphs("inference_gnn_data_VagusA6050_HC0_AS1_8.dgl", [g])
+logging.info("Graph saved to inference_gnn_data_VagusA6050_HC0_AS1_8.dgl")
 
